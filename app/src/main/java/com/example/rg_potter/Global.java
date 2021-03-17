f07f48d0cbb3c57faee22e5e87bf60f3323c7d9c
Felipe Beserra <49963588+Beserrovsky@users.noreply.github.com>
2021-03-17 15:35:32 -0300

    Basic setup and Logo export

---
+++ app/src/main/java/com/example/rg_potter/Global.java
@@ -0,0 +1,5 @@
+package com.example.rg_potter;
+
+final public class Global {
+    public static final String API_URL = "https://harry-potter-deploy.herokuapp.com";
+}
--- app/src/main/java/com/example/rg_potter/MainActivity.java
+++ app/src/main/java/com/example/rg_potter/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rg_potter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+    }
+
+    private void getPotions(){
+        // Fetch Global.API_URL + "endpoint"
+
+        // SQLITE
+
+
     }
 }
