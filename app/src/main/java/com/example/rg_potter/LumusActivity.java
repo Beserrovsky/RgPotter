508209d2e0f944eaf8a6bc535c222e2133141e9a
Beserrovsky <=>
2021-06-14 23:24:13 -0300

    Location and some other stuff

--- app/src/main/java/com/example/rg_potter/LumusActivity.java
+++ app/src/main/java/com/example/rg_potter/LumusActivity.java
@@ -2,13 +2,106 @@ package com.example.rg_potter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
-public class LumusActivity extends AppCompatActivity {
+public class LumusActivity extends AppCompatActivity implements SensorEventListener {
+
+    AlertDialog alertDialog;
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
+    private boolean notAble;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lumus);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+
+        // Use the accelerometer.
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null){
+            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        } else{
+            notAble = true;
+        }
+
+        if(notAble) noSensors();
     }
+
+    private boolean isRunning = false;
+    public void Start(View view){
+        if(notAble) noSensors();
+        else isRunning = true;
+    }
+
+    private float[] initValues = new float[3];
+    private boolean init = true, mode = false;
+    private float THRESHOLD = 1000;
+    public void onSensorChanged(SensorEvent event){
+
+        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            if(init) {
+                initValues[0] = event.values[0];
+                initValues[1] = event.values[1];
+                initValues[2] = event.values[2];
+                init = false;
+            }
+
+            boolean done = false;
+
+            for(int i = 0; i < initValues.length; i++){
+                if(done) break;
+                if(event.values[i] > initValues[i] + THRESHOLD) done = true;
+            }
+
+            if(done){
+                mode = !mode;
+                setLight(mode);
+                init = true;
+            }
+        }
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    private void noSensors(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getResources().getString(R.string.no_sensor_lumus_title));
+        builder.setMessage(getString(R.string.no_sensor_lumus_desc));
+        alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    String cameraId;
+    CameraManager camManager;
+    private void setLight(boolean status){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            cameraId = null;
+
+            try {
+                cameraId = camManager.getCameraIdList()[0];
+                camManager.setTorchMode(cameraId, status);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/rg_potter/MainActivity.java
+++ app/src/main/java/com/example/rg_potter/MainActivity.java
@@ -1,21 +1,38 @@
 package com.example.rg_potter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.rg_potter.data.Global;
 import com.example.rg_potter.entity.User;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnSuccessListener<Location>, OnFailureListener {
 
+    Location location;
+    Address address;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Log.d("MainActivity", "Activity called");
@@ -39,7 +56,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Global.user = new User(0, this);
+
         setContentView(R.layout.activity_main);
+
+        if(address==null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                final FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+                fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this);
+                fusedLocationProviderClient.getLastLocation().addOnFailureListener(this);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                Log.d("MainActivity", "Permissão para localização não concedida, utilizando Brasil!");
+                this.location = new Location("me");
+                this.location.setLatitude(-23.520835998942797);
+                this.location.setLongitude(-46.72865548277011);
+                defineAddress(getAddress(this.location.getLatitude(),this.location.getLongitude()));
+            }
+        }
     }
 
     public void Visualization (View view){
@@ -55,6 +88,41 @@ public class MainActivity extends AppCompatActivity {
     public void More (View view){
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://harrypotter.fandom.com/pt-br/wiki/P%C3%A1gina_Principal"));
         startActivity(intent);
+    }
+
+    @Override
+    public void onFailure(@NonNull @org.jetbrains.annotations.NotNull Exception e) {
+        Log.d("MainActivity", "Erro ao localizar dispositivo, utilizando Brasil!");
+        this.location = new Location("me");
+        this.location.setLatitude(-23.520835998942797);
+        this.location.setLongitude(-46.72865548277011);
+        defineAddress(getAddress(this.location.getLatitude(),this.location.getLongitude()));
+    }
+
+    @Override
+    public void onSuccess(Location location) {
+        if(location != null) {
+            this.location = location;
+            defineAddress(getAddress(this.location.getLatitude(),this.location.getLongitude()));
+        }
+    }
+
+    private void defineAddress(String countryName){((TextView) findViewById(R.id.txtCountry)).setText(countryName);}
+
+    private String getAddress(double latitude, double longitude) {
+        StringBuilder result = new StringBuilder();
+        try {
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            if (addresses.size() > 0) {
+                Address address = addresses.get(0);
+                //result.append(address.getLocality()).append("\n");
+                result.append(address.getCountryName());
+            }
+        } catch (IOException e) {
+            Log.e("tag", e.getMessage());
+        }
+        return result.toString();
     }
 
 }
