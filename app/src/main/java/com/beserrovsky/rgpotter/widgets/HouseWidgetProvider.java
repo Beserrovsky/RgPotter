2acdb7ed9d06175a82cede45677e23ce9ffd5ce2
Felipe Beserra <felipebeserra25@gmail.com>
2021-09-20 18:01:27 -0300

    Logo change

--- app/src/main/java/com/beserrovsky/rgpotter/widgets/HouseWidgetProvider.java
+++ app/src/main/java/com/beserrovsky/rgpotter/widgets/HouseWidgetProvider.java
@@ -1,22 +1,28 @@
 package com.beserrovsky.rgpotter.widgets;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.beserrovsky.rgpotter.MainActivity;
+import com.beserrovsky.rgpotter.R;
 
 public class HouseWidgetProvider extends AppWidgetProvider {
 
-//    @Override
-//    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-//        super.onUpdate(context, appWidgetManager, appWidgetIds);
-//
-//        final int amount = appWidgetIds.length;
-//        for (int i = 0; i < amount; i++) {
-//            // TODO: Intent intent = new Intent(context, ACTIVITY);
-//
-//            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-//
-//            // TODO: RemoteViews views = new RemoteViews(context.getPackageName(), LAYOUT_ID);
-//            views.setOnClickPendingIntent(R.id.imageView3, pendingIntent);
-//            appWidgetManager.updateAppWidget(appWidgetIds[i], views);
-//        }
-//    }
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        for (int appWidgetId : appWidgetIds) {
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_house_gryffindor);
+            views.setOnClickPendingIntent(R.id.widget_gryffindor_image, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
 }
