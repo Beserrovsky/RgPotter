b15b8b8ab5ce96dd9668235d691db181f8e74eda
Felipe Beserra <felipebeserra25@gmail.com>
2021-09-20 13:55:15 -0300

    Nav change handling

--- app/src/main/java/com/beserrovsky/rgpotter/MainActivity.java
+++ app/src/main/java/com/beserrovsky/rgpotter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.beserrovsky.rgpotter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -12,9 +13,9 @@ import com.beserrovsky.rgpotter.ui.curiosity.CuriosityFragment;
 import com.beserrovsky.rgpotter.ui.rg.RgFragment;
 import com.beserrovsky.rgpotter.ui.spells.SpellFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity{
 
     BottomNavigationView navigationView;
     private boolean viewIsAtHome;
@@ -23,14 +24,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         navigationView = (BottomNavigationView) findViewById(R.id.bottom_nav_view);
-        displayView(R.id.nav_rg);
-        navigationView.bringToFront();
-    }
 
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        displayView(item.getItemId());
-        return true;
+        // Thanks Guilherme Palma, https://github.com/GuilhermePalma <3
+        navigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                displayView(item.getItemId());
+                return true;
+            }
+        });
+
+        displayView(R.id.nav_rg);
     }
 
     // HUGE Thanks to Ojonugwa Jude Ochalifu on https://stackoverflow.com/questions/32944798/switch-between-fragments-with-onnavigationitemselected-in-new-navigation-drawer
@@ -40,21 +44,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Fragment fragment = null;
         String title = getString(R.string.app_name);
 
-        switch (viewId) {
-            case R.id.nav_rg:
-                fragment = new RgFragment();
-                title  = getString(R.string.rg_name);
-                viewIsAtHome = true;
-            case R.id.nav_spells:
-                fragment = new SpellFragment();
-                title = getString(R.string.spell_name);
-                viewIsAtHome = false;
-                break;
-            case R.id.nav_curiosity:
-                fragment = new CuriosityFragment();
-                title = getString(R.string.curiosity_name);
-                viewIsAtHome = false;
-                break;
+        // TODO: SWITCH
+        if (viewId == R.id.nav_rg) {
+            fragment = new RgFragment();
+            title  = getString(R.string.rg_name);
+            viewIsAtHome = true;
+        }
+        if (viewId == R.id.nav_spells) {
+            fragment = new SpellFragment();
+            title = getString(R.string.spell_name);
+            viewIsAtHome = false;
+        }
+        if (viewId == R.id.nav_curiosity) {
+            fragment = new CuriosityFragment();
+            title = getString(R.string.curiosity_name);
+            viewIsAtHome = false;
         }
 
         if (fragment != null) {
@@ -71,10 +75,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public void onBackPressed() {
-        if (!viewIsAtHome) { //if the current view is not the News fragment
-            displayView(R.id.nav_rg); //display the News fragment
+        if (!viewIsAtHome) { //if the current view is not the RG fragment
+            displayView(R.id.nav_rg); //display the RG fragment
         } else {
-            moveTaskToBack(true);  //If view is in News fragment, exit application
+            moveTaskToBack(true);  //If view is in RG fragment, exit application
         }
     }
 
