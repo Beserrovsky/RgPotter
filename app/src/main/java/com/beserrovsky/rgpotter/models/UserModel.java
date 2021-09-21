c93143204195015d83c1aa1db637bd4b348760a7
Felipe Beserra <felipebeserra25@gmail.com>
2021-09-21 16:03:55 -0300

    User Model created

--- app/src/main/java/com/beserrovsky/rgpotter/models/UserModel.java
+++ app/src/main/java/com/beserrovsky/rgpotter/models/UserModel.java
@@ -1,5 +1,20 @@
 package com.beserrovsky.rgpotter.models;
 
 public class UserModel {
-
+    public String Email;
+    public String Name;
+    public String House_ID;
+    public House House;
+    class House {
+        public String ID;
+        public String Name;
+    }
+    public String Pronoum;
+    public Gender Gender;
+    class Gender {
+        public String Pronoum;
+    }
+    public String Password;
+    public int lumusSuccess;
+    public int lumusFails;
 }
